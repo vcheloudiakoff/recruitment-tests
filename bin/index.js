cc4e1e7a63dbc4428471e580f1d82cc6390efb64
vcheloudiakoff <vcheloudiakoff@gmail.com>
2020-09-14 12:50:18 +0200

    Arg errors, test implementation needed

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,29 @@
 #!/usr/bin/env node
 
 params = process.argv.pop();
-console.log(params);
-if (params.split("=")[0] === "--filter") {
+console.log("params = ", params);
+console.log("params splitted = ", params.split("=")[0]);
+if (process.argv.length >= 3) {
+    console.log("You should have only one option");
+    console.log("Options: --filter=[PARAMETER] or --count");
+    return
+}
+if (params.split("=")[0] !== "--filter" && params !== "--count") {
+    console.log("Usage: node bin/index.js [ARG] ");
+    console.log("ARG: --filter=[PARAMETER] or --count");
+    return
+} else {
+    if (params === "--filter" || getFilterArgument(params) === "") {
+        console.log("Filter usage: --filter=[PARAMETER]");
+        console.log("And this need to have one parameter");
+        return
+    }
+    if (params === "--count") {
+        console.log('Count option OK');
+        return
+    }
     arg = getFilterArgument(params);
-    console.log(arg);
-} else if (params.split("=")[0] === "--count") {
-    console.log("Count option");
+    console.log("arg= ", arg);
 }
 
 function getFilterArgument(params) {
