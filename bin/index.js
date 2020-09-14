1e6f73589d0af8608a45cb22ef901a2c772c8e57
vcheloudiakoff <vcheloudiakoff@gmail.com>
2020-09-14 12:15:15 +0200

    Select options from CLI

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log(process.argv);
+params = process.argv.pop();
+console.log(params);
+if (params.split("=")[0] === "--filter") {
+    arg = getFilterArgument(params);
+    console.log(arg);
+} else if (params.split("=")[0] === "--count") {
+    console.log("Count option");
+}
+
+function getFilterArgument(params) {
+    return params.split('=')[1];
+}
