d917df24999d9175a57c19fa2577b47fab7815e1
vcheloudiakoff <vcheloudiakoff@gmail.com>
2020-09-15 11:57:59 +0200

    Inputs error fixed, need bonus --count option

--- bin/index.js
+++ bin/index.js
@@ -1,37 +1,46 @@
 #!/usr/bin/env node
-var data = require("../data.js").data;
-var params = process.argv;
-var error = "";
+var data = require("../data.js").data
+var params = process.argv
+var filter = ""
+var count = ""
+var error = ""
 
 function getFilterArgument(params) {
-    return params.split('=')[1];
+    return params.split('=')[1]
 }
 
 function app() {
-    if (process.argv.length != 3) {
-        console.log("You should have only one option");
-        console.log("Options: --filter=[PARAMETER] or --count");
-        error = "Options error";
-        return error;
+    if (process.argv.length < 3 || process.argv.length > 4) {
+        console.log("You should have only one or two options")
+        console.log("Options: --filter=[PARAMETER] or/and --count")
+        error = "Options error"
+        return error
     }
-    params = params.pop();
-    if ((params.split("=")[0] !== "--filter" && params !== "--count")) {
-        console.log("Usage: node bin/index.js [ARG] ");
-        console.log("ARG: --filter=[PARAMETER] or --count");
-        error = "Usage";
-        return error;
+    filter = params.filter(x => x.includes("--filter"))
+    count = params.filter(x => x.includes("--count"))
+    filter = filter.pop()
+    count = count.pop()
+    console.log(filter)
+    console.log(count)
+    if (typeof filter === "undefined") {
+        console.log("Usage: node bin/index.js [ARG] ")
+        console.log("ARG: --filter=[PARAMETER] or --count")
+        error = "Usage"
+        return error
+    }
+    if (typeof count === "undefined" && process.argv.length === 4) {
+        console.log("Usage: node bin/index.js [ARG] ")
+        console.log("ARG: --filter=[PARAMETER] or --count")
+        error = "Usage"
+        return error
     } else {
-        if (params === "--filter" || getFilterArgument(params) === "") {
-            console.log("Filter usage: --filter=[PARAMETER]");
-            console.log("And this need to have one parameter");
-            error = "Filter usage";
-            return error;
+        if (filter === "--filter" || filter.split("=")[1] === "") {
+            console.log("Filter usage: --filter=[PARAMETER]")
+            console.log("And this need to have one parameter")
+            error = "Filter usage"
+            return error
         }
-        if (params === "--count") {
-            console.log('Count option OK');
-            return
-        }
-        arg = getFilterArgument(params);
+        arg = filter.split("=")[1]
         let final = []
         data.forEach(country => {
             let animalInCountry = false
@@ -65,10 +74,15 @@ function app() {
                 })
             }
         })
-        final.forEach(x => console.log(JSON.stringify(x, null, 2)))
+        if (typeof count !== 'undefined') {
+            console.log('Count option OK')
+            final.forEach(x => console.log(JSON.stringify(x, null, 2)))
+        } else {
+            final.forEach(x => console.log(JSON.stringify(x, null, 2)))
+        }
     }
 }
 
-app();
+app()
 
-module.exports = app;
+module.exports = app
